65b1c6671c792eb2b4112ee33066a0ba7998121d
hk-cogoport <kumarharshit8090@gmail.com>
2023-11-17 21:09:05 +0530

    prettify

--- app/controllers/currency_conversion_controller.rb
+++ app/controllers/currency_conversion_controller.rb
@@ -1,24 +1,28 @@
 class CurrencyConversionController < ApplicationController  
-    def show
-      @from_currency = params[:from_currency]
-      @to_currency = params[:to_currency]
-  
-      if @from_currency.present? && @to_currency.present?
-        @exchange_rate = CurrencyLayer.get_conversion_rates(@from_currency, @to_currency)
 
-        if @exchange_rate.present? &&  @exchange_rate['success']==true
-          if request.format.html?
-            @amount = params[:amount].to_f
+  def index
+  end
 
-            @converted_amount = @amount * @exchange_rate['quotes'].values.first
-          else
-            render json: @converted_amount
-          end
+  def show
+    @from_currency = params[:from_currency]
+    @to_currency = params[:to_currency]
+
+    if @from_currency.present? && @to_currency.present?
+      @exchange_rate = CurrencyLayer.get_conversion_rates(@from_currency, @to_currency)
+
+      if @exchange_rate.present? &&  @exchange_rate['success']==true
+        if request.format.html?
+          @amount = params[:amount].to_f
+
+          @converted_amount = @amount * @exchange_rate['quotes'].values.first
         else
-          flash.now[:alert] = 'Failed to fetch currency conversion data. Please try again.'
+          render json: @converted_amount
         end
       else
-        flash.now[:alert] = 'Both source and target currencies are required.'
+        flash.now[:alert] = 'Failed to fetch currency conversion data. Please try again.'
       end
+    else
+      flash.now[:alert] = 'Both source and target currencies are required.'
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,9 @@ Rails.application.routes.draw do
 
   get '/login', to: 'session#new'
 
-  post '/currency_conversion', to: 'currency_conversion#show'
-  get '/currency_conversion', to: 'currency_conversion#show'
+  # post '/currency_conversion', to: 'currency_conversion#show'
+  get '/currency_conversion', to: 'currency_conversion#index'
+  post '/currency_conversions', to: 'currency_conversion#show', as: 'convert_currency'
 
   # Defines the root path route ("/")
   root 'static_page#home'
